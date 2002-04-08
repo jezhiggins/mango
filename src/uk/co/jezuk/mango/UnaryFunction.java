8af1a5f9156ab2f379cc6c6e6eeb5e4356428982
jez <>
2002-04-08 11:45:55 +0000

    more javadoc

--- src/uk/co/jezuk/mango/UnaryFunction.java
+++ src/uk/co/jezuk/mango/UnaryFunction.java
@@ -1,9 +1,9 @@
 package uk.co.jezuk.mango;
 
 /**
- * A <code>Generator</code> describes a function which takes no arguments - <code>fn()</code>.
+ * A <code>Generator</code> describes a function which takes one argument - <code>fn(x)</code>.
  * It returns some <code>Object</code>, and may return the same object or different objects for
- * each invocation.  It can refer to local state, perform disk reads or writes, or whatever.  
+ * each invocation, even given the same argument.  It can refer to local state, perform disk reads or writes, or whatever.  
  * @author Jez Higgins, jez@jezuk.co.uk
  * @version $Id$
  */
