1d9645d052f045123626595edc30f6eb1c70b17f
jez <>
2002-04-08 13:44:32 +0000

    javadoc fixes

--- src/uk/co/jezuk/mango/Predicate.java
+++ src/uk/co/jezuk/mango/Predicate.java
@@ -3,7 +3,8 @@ package uk.co.jezuk.mango;
 /**
  * A <code>Predicate</code> is a special form of a <code>UnaryFunction</code>, whose result represents the
  * truth or otherwise of some condition.
- * @returns a <code>true</code> if the condition the Predicate tests for is satisfied, <code>false</code> otherwise.
+ * <br>
+ * Returns <code>true</code> if the condition the Predicate tests for is satisfied, <code>false</code> otherwise.
  * @author Jez Higgins, jez@jezuk.co.uk
  * @version $Id$
  */
