1a2ecd9c386167e4e8d110ac3c82812b1f23f3c3
jez <>
2002-04-08 11:46:51 +0000

    *** empty log message ***

--- src/uk/co/jezuk/mango/BinaryFunction.java
+++ src/uk/co/jezuk/mango/BinaryFunction.java
@@ -2,7 +2,7 @@ package uk.co.jezuk.mango;
 
 /**
  * A <code>BinaryFunction</code> is some function taking two arguments -
- * <code>fn(x, y)</code>
+ * <code>fn(x, y)</code>.
  * It returns some <code>Object</code>, and may return the same object or different objects for
  * each invocation, even given the same arguments.  It can refer to local state, perform disk reads or writes, or whatever.  
  * @author Jez Higgins, jez@jezuk.co.uk
