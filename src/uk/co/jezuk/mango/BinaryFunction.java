7751a5b2db07849901865071e45cefb3825318da
jez <>
2002-04-08 09:22:44 +0000

    *** empty log message ***

---
+++ src/uk/co/jezuk/mango/BinaryFunction.java
@@ -0,0 +1,12 @@
+package uk.co.jezuk.mango;
+
+/**
+ * 
+ * @author Jez Higgins, jez@jezuk.co.uk
+ * @version $Id$
+ */
+public interface BinaryFunction
+{
+    abstract public void fn(Object o1, Object o2);
+} // BinaryFunction
+
---
+++ src/uk/co/jezuk/mango/UnaryFunction.java
@@ -0,0 +1,12 @@
+package uk.co.jezuk.mango;
+
+/**
+ * 
+ * @author Jez Higgins, jez@jezuk.co.uk
+ * @version $Id$
+ */
+public interface UnaryFunction
+{
+    abstract public void fn(Object o);
+} // UnaryFunction
+
