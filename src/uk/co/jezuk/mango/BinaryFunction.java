5d5952abb127416fadddd1015cf4f6bdb9ec6206
jez <>
2002-04-08 18:10:16 +0000

    moved binders out to Bind

--- src/uk/co/jezuk/mango/BinaryFunction.java
+++ src/uk/co/jezuk/mango/BinaryFunction.java
@@ -11,53 +11,5 @@ package uk.co.jezuk.mango;
 public interface BinaryFunction
 {
   abstract public Object fn(Object x, Object y);
-
-  /**
-   * Adapts a <code>BinaryFunction</code> into a <code>UnaryFunction</code>.
-   * If <code>f</code> an object implementing <code>BinaryFunction</code>, then
-   * <code>BindFirst(f, C).fn(arg)</code> returns <code>f(C, arg)</code>.<p>
-   * Intuitively, you can think of this as "binding" the first argument of a 
-   * <code>BinaryFunction</code> to a constant, thus giving a <code>UnaryFunction</code>.
-   */
-  public class BindFirst implements UnaryFunction
-  {
-    public BindFirst(BinaryFunction f, Object c)
-    {
-      fn_ = f;
-      c_ = c;
-    } // BindFirst
-
-    public Object fn(Object arg)
-    {
-      return fn_.fn(c_, arg);
-    } // fn
-
-    private BinaryFunction fn_;
-    private Object c_;
-  } // BindFirst
-
-  /**
-   * Adapts a <code>BinaryFunction</code> into a <code>UnaryFunction</code>.
-   * If <code>f</code> an object implementing <code>BinaryFunction</code>, then
-   * <code>BindSecond(f, C).fn(arg)</code> returns <code>f(arg, C)</code>.<p>
-   * Intuitively, you can think of this as "binding" the second argument of a 
-   * <code>BinaryFunction</code> to a constant, thus giving a <code>UnaryFunction</code>.
-   */
-  public class BindSecond implements UnaryFunction
-  {
-    public BindSecond(BinaryFunction f, Object c)
-    {
-      fn_ = f;
-      c_ = c;
-    } // BindSecond
-
-    public Object fn(Object arg)
-    {
-      return fn_.fn(arg, c_);
-    } // fn
-
-    private BinaryFunction fn_;
-    private Object c_;
-  } // BindSecond
 } // BinaryFunction
 
