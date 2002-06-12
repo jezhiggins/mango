360a98df2e555203a5dea549497803e69039b628
jez <>
2002-06-12 11:18:03 +0000

    *** empty log message ***

---
+++ src/uk/co/jezuk/mango/unarypredicates/And.java
@@ -0,0 +1,13 @@
+package uk.co.jezuk.mango.unarypredicates;
+
+/**
+ * @version $Id$
+ */
+public class And implements uk.co.jezuk.mango.Predicate
+{
+  public And(uk.co.jezuk.mango.Predicate pred1, uk.co.jezuk.mango.Predicate pred2) { p1_ = pred1; p2_ = pred2; }
+  public boolean test(Object x) { return p1_.test(x) && p2_.test(x); }
+
+  private uk.co.jezuk.mango.Predicate p1_;
+  private uk.co.jezuk.mango.Predicate p2_;
+} // And
