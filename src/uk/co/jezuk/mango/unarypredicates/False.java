3cefdae359fe385dcc43cbea0a12134c6e87838b
jez <>
2002-06-12 10:21:06 +0000

    a predicate which always returns false

---
+++ src/uk/co/jezuk/mango/unarypredicates/False.java
@@ -0,0 +1,9 @@
+package uk.co.jezuk.mango.unarypredicates;
+
+/**
+ * @version $Id$
+ */
+public class False implements uk.co.jezuk.mango.Predicate
+{
+  public boolean test(Object x) { return false; }
+} // False
