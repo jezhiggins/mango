34fb7337b76751160141abfe0721a58586fc962d
jez <>
2002-06-12 10:20:48 +0000

    a predicate which always returns true

---
+++ src/uk/co/jezuk/mango/unarypredicates/True.java
@@ -0,0 +1,9 @@
+package uk.co.jezuk.mango.unarypredicates;
+
+/**
+ * @version $Id$
+ */
+public class True implements uk.co.jezuk.mango.Predicate
+{
+    public boolean test(Object x) { return true; }
+} // True
