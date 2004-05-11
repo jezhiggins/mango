45377a7b759cae2a21e6392809734c5df34b7e95
jez <>
2004-05-11 08:11:55 +0000

    Factory interface to generators

---
+++ src/uk/co/jezuk/mango/Generators.java
@@ -0,0 +1,18 @@
+package uk.co.jezuk.mango;
+
+/**
+ * The Mango Library.
+ *
+ * @author Jez Higgins, jez@jezuk.co.uk
+ * @version $Id$
+ */
+public class Generators
+{
+  /**
+   * Returns Integer(seed), Integer(seed+1), Integer(seed+2) ... on successive calls to fn()
+   * Sequence begins at 0 if no seed value is given.
+   */
+  static Generator IntegerSequence() { return new uk.co.jezuk.mango.generators.IntegerSequence(); }
+  static Generator IntegerSequence(int seed) { return new uk.co.jezuk.mango.generators.IntegerSequence(seed); }
+  static Generator IntegerSequence(Integer seed) { return new uk.co.jezuk.mango.generators.IntegerSequence(seed); }
+} // class Generators
