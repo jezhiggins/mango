53ccc24c9a3330e25ef7b2bb19eed855f2c1fe5d
jez <jez@isla>
2009-10-14 23:50:43 +0100

    removed $Id$ nonsense

--- src/uk/co/jezuk/mango/UnaryFunction.java
+++ src/uk/co/jezuk/mango/UnaryFunction.java
@@ -5,7 +5,6 @@ package uk.co.jezuk.mango;
  * It returns some <code>T</code>, and may return the same object or different objects for
  * each invocation, even given the same argument.  It can refer to local state, perform disk reads or writes, or whatever.  
  * @author Jez Higgins, jez@jezuk.co.uk
- * @version $Id$
  */
 public interface UnaryFunction<T, R>
 {
