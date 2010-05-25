4514892ec0fbc533c96a52782dabd5a57f91ace7
jez <jez@isla>
2010-05-25 09:34:39 +0100

    Added Xor and OneOf

--- src/uk/co/jezuk/mango/Predicates.java
+++ src/uk/co/jezuk/mango/Predicates.java
@@ -68,22 +68,34 @@ public class Predicates
   static public <T> Predicate<T> Or(Predicate<T> pred1, Predicate<T> pred2) { return new Or<T>(pred1, pred2); }
 
   /**
-   * A <code>Predicate</code> which returns <code>true</code> if all of the supplied <code>Predicate</code>s are true.  It is an AND generalised to any number of arguments.  The <code>Predicate</code>s are evaluated in the order supplied.
+   * A <code>Predicate<T></code> which returns the logical XOR of two other <code>Predicate<T></code>.
+   */
+  static public <T> Predicate<T> Xor(Predicate<T> pred1, Predicate<T> pred2) { return new Xor<T>(pred1, pred2); }
+
+  /**
+   * A <code>Predicate</code> which returns <code>true</code> if all of the supplied <code>Predicate</code>s are true.  It is an AND generalised to any number of arguments.  The <code>Predicate</code>s are evaluated in the order supplied.  Evaluation is short circuited as soon as a predicate fails.
    */
   static public <T> Predicate<T> All(Object... preds) { return All(new ArrayIterator<Predicate<T>>(preds)); }
   static public <T> Predicate<T> All(Collection<Predicate<T>> preds) { return All(preds.iterator()); }
   static public <T> Predicate<T> All(Iterator<Predicate<T>> preds) { return new All<T>(preds); }
 
   /**
-   * A <code>Predicate</code> which returns <code>true</code> if any one of the supplied <code>Predicate</code>s are true.  It is an OR generalised to any number of arguments.  The <code>Predicate</code>s are evaluated in the order supplied.
+   * A <code>Predicate</code> which returns <code>true</code> if any one of the supplied <code>Predicate</code>s are true.  It is an OR generalised to any number of arguments.  The <code>Predicate</code>s are evaluated in the order supplied. Evaluation short circuits as soon as a predicate returns true.
    */
   static public <T> Predicate<T> Any(Object... preds) { return Any(new ArrayIterator<Predicate<T>>(preds)); }
   static public <T> Predicate<T> Any(Collection<Predicate<T>> preds) { return Any(preds.iterator()); }
   static public <T> Predicate<T> Any(Iterator<Predicate<T>> preds) { return new Any<T>(preds); }
 
   /**
+   * A <code>Predicate</code> which returns <code>true</code> if only one of the supplied <code>Predicate</code>s is true.  The <code>Predicate</code>s are evaluated in the order supplied.  Execution will short circuit as soon as more than one Predicate has returned true.
+   */
+  static public <T> Predicate<T> OneOf(Object... preds) { return OneOf(new ArrayIterator<Predicate<T>>(preds)); }
+  static public <T> Predicate<T> OneOf(Collection<Predicate<T>> preds) { return OneOf(preds.iterator()); }
+  static public <T> Predicate<T> OneOf(Iterator<Predicate<T>> preds) { return new OneOf<T>(preds); }
+
+  /**
    * A <code>Predicate<T></code> which checks whether the passed object 
-   * reference is null.
+   * reference is null. 
    */
   static public <T> Predicate<T> IsNull()
   {
